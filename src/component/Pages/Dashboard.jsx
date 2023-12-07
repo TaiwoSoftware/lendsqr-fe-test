4339201a9a45b0995ad76a82b23d768d1cc3ef4f
Taiwo AKerele <davcodes24@gmail.com>
2023-12-07 22:09:27 +0100

    still working on the dashboard branch

--- src/component/Pages/Dashboard.jsx
+++ src/component/Pages/Dashboard.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import Logo from '../Images/Logo.svg';
-import searchIcon from '../Images/searchicon.svg';
-import notifier from '../Images/notifier.svg';
-import avatar from '../Images/avatar.svg';
+import React from "react";
+import Logo from "../Images/Logo.svg";
+import searchIcon from "../Images/searchicon.svg";
+import notifier from "../Images/notifier.svg";
+import avatar from "../Images/avatar.svg";
+import dropdown from "../Images/Vector.svg";
 const Dashboard = () => {
-    return (
-        <div className='dashboard'>
-            <nav>
-                <img src={Logo} alt='Logo' />
-                <input type='search' placeholder='search for anything' />
-                <button>
-                    <img src={searchIcon} alt='Search icon' />
-                </button>
-                <div>
-                    <p>Docs</p>
-                    <img src={notifier} alt='notifier' />
-                    <img src={avatar} alt='avatar' />
-                </div>
-            </nav>
+  return (
+    <div className="dashboard">
+      <nav>
+        <img src={Logo} alt="Logo" />
+        <div className="searchInput">
+          <input type="search" placeholder="search for anything" />
+          <button>
+            <img src={searchIcon} alt="Search icon" />
+          </button>
         </div>
-    );
-}
+        <div className="userDetails">
+          <p>Docs</p>
+          <img src={notifier} alt="notifier" />
+          <img src={avatar} alt="avatar" />
+          <p>Adedeji</p>
+          <img src={dropdown} alt="dropdown menu" />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Dashboard;
