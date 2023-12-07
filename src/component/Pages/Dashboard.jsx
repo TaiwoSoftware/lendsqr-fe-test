7c29e8a921cfba368df3241a7b830828bfca4d0f
Taiwo AKerele <davcodes24@gmail.com>
2023-12-07 14:19:59 +0100

    Working on the dashboard feature

--- src/component/Pages/Dashboard.jsx
+++ src/component/Pages/Dashboard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import Logo from '../Images/Logo.svg';
+import searchIcon from '../Images/searchicon.svg';
+import notifier from '../Images/notifier.svg';
 const Dashboard = () => {
     return (
-        <div>
-            <p>This is the Dashboard</p>
+        <div className='dashboard'>
+            <nav>
+                <img src={Logo} alt='Logo' />
+                <input type='search' placeholder='search for anything' />
+                <button>
+                    <img src={searchIcon} alt='Search icon' />
+                </button>
+                <div>
+                    <p>Docs</p>
+                    <img src={notifier} alt='notifier' />
+                </div>
+            </nav>
         </div>
     );
 }
