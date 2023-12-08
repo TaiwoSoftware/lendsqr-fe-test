d539336e07b78b8349a7746d94a22901df4be55b
Taiwo AKerele <davcodes24@gmail.com>
2023-12-08 10:47:45 +0100

    done with the nav for the dashboard

--- src/component/Pages/Dashboard.jsx
+++ src/component/Pages/Dashboard.jsx
@@ -11,15 +11,15 @@ const Dashboard = () => {
         <img src={Logo} alt="Logo" />
         <div className="searchInput">
           <input type="search" placeholder="search for anything" />
-          <button>
+          <button type="submit" className="submitButton">
             <img src={searchIcon} alt="Search icon" />
           </button>
         </div>
         <div className="userDetails">
-          <p>Docs</p>
+          <p className="docs">Docs</p>
           <img src={notifier} alt="notifier" />
           <img src={avatar} alt="avatar" />
-          <p>Adedeji</p>
+          <p className="adedji">Adedeji</p>
           <img src={dropdown} alt="dropdown menu" />
         </div>
       </nav>
